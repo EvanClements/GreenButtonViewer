4c72cd0b1ba115152400bd3f051ef7c089173a3b
Evan Clements <me@evanclements.co>
2021-08-24 16:33:43 -0400

    Moving stuff around, setting up Kemal Spec Testing

--- main.cr
+++ main.cr
@@ -2,31 +2,6 @@ require "xml"
 require "json"
 require "kemal"
 
-xml = <<-XML
-	<Account AcountNumber="30011890917">
-		<ServicePoint ServicePointID="2300378177" MeterID="G044598305">
-			<Channel ChannelNumber="10">
-				<ReadingData>
-					<DateTime>3/20/2020 12:00:00 AM</DateTime>
-					<Value>0.193</Value>
-				</ReadingData>
-                <ReadingData>
-                  <DateTime>3/20/2020 1:00:00 AM</DateTime>
-                  <Value>0.476</Value>
-                </ReadingData>
-                <ReadingData>
-                  <DateTime>3/20/2020 2:00:00 AM</DateTime>
-                  <Value>0.206</Value>
-                </ReadingData>
-                <ReadingData>
-                  <DateTime>3/20/2020 3:00:00 AM</DateTime>
-                  <Value>0.126</Value>
-                </ReadingData>
-			</Channel>
-		</ServicePoint>
-	</Account>
-XML
-
 document = XML.parse(xml).first_element_child
 
 # When you access the root, parse the preset XML object and spit out JSON

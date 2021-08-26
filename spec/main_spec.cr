92c20f7bce1615d625b1ea73a0a0bf71a186a980
Evan Clements <me@evanclements.co>
2021-08-26 20:11:47 +0000

    Added xml variable with xml data

--- spec/main_spec.cr
+++ spec/main_spec.cr
@@ -5,30 +5,7 @@ describe "Your::Kemal::App" do
   # You can use get,post,put,patch,delete to call the corresponding route.
   it "renders /" do
     post "/"
-    xml = <<-XML
-        <Account AcountNumber="30011890917">
-            <ServicePoint ServicePointID="2300378177" MeterID="G044598305">
-                <Channel ChannelNumber="10">
-                    <ReadingData>
-                        <DateTime>3/20/2020 12:00:00 AM</DateTime>
-                        <Value>0.193</Value>
-                    </ReadingData>
-                    <ReadingData>
-                    <DateTime>3/20/2020 1:00:00 AM</DateTime>
-                    <Value>0.476</Value>
-                    </ReadingData>
-                    <ReadingData>
-                    <DateTime>3/20/2020 2:00:00 AM</DateTime>
-                    <Value>0.206</Value>
-                    </ReadingData>
-                    <ReadingData>
-                    <DateTime>3/20/2020 3:00:00 AM</DateTime>
-                    <Value>0.126</Value>
-                    </ReadingData>
-                </Channel>
-            </ServicePoint>
-        </Account>
-    XML
+    
     response.body.should eq "Hello World!"
   end
 

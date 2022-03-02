d72b6e0147989c8f209479e0156fbb0ed6141e2d
Evan Clements <me@evanclements.co>
2022-03-02 14:12:23 -0500

    Updated main.cr flow

--- main.cr
+++ main.cr
@@ -2,32 +2,42 @@ require "xml"
 require "json"
 require "kemal"
 
-xml = <<-XML
-        <Account AcountNumber="30011890917">
-            <ServicePoint ServicePointID="2300378177" MeterID="G044598305">
-                <Channel ChannelNumber="10">
-                    <ReadingData>
-                        <DateTime>3/20/2020 12:00:00 AM</DateTime>
-                        <Value>0.193</Value>
-                    </ReadingData>
-                    <ReadingData>
-                    <DateTime>3/20/2020 1:00:00 AM</DateTime>
-                    <Value>0.476</Value>
-                    </ReadingData>
-                    <ReadingData>
-                    <DateTime>3/20/2020 2:00:00 AM</DateTime>
-                    <Value>0.206</Value>
-                    </ReadingData>
-                    <ReadingData>
-                    <DateTime>3/20/2020 3:00:00 AM</DateTime>
-                    <Value>0.126</Value>
-                    </ReadingData>
-                </Channel>
-            </ServicePoint>
-        </Account>
-    XML
+# Created new DB channel/file
+# Create tempfile for uploaded file to be saved
+# determine if file is CSV or XML or Other
+# If CSV, do parseCsv
+# If XML do parseXml
+# The only difference is the way the file is navigated
+# Do parseGreenButton with "file" as needed param and 
 
-document = XML.parse(xml).first_element_child
+require "xml"
+
+xml = <<-XML
+<Account AcountNumber="30011890917">
+     <ServicePoint ServicePointID="2300378177" MeterID="G044598305">
+          <Channel ChannelNumber="10">
+               <ReadingData>
+                    <DateTime>11/1/2021 12:00:00 AM</DateTime>
+                    <Value>0.186</Value>
+               </ReadingData>
+               <ReadingData>
+                    <DateTime>11/1/2021 1:00:00 AM</DateTime>
+                    <Value>0.209</Value>
+               </ReadingData>
+               <ReadingData>
+                    <DateTime>11/1/2021 2:00:00 AM</DateTime>
+                    <Value>0.458</Value>
+               </ReadingData>
+          </Channel>
+     </ServicePoint>
+</Account>
+XML
+
+document = XML.parse(xml)
+
+puts "Account Number: " + document.children[0]["AcountNumber"] + "\n"
+child = document.children
+puts child[0]
 
 # When you access the root, parse the preset XML object and spit out JSON
 get "/" do |env|
